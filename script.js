f71d91f0852eacdc7ad172ac19be9ece6c680438
RexxMK <rikkekodahl@live.dk>
2023-04-14 13:32:59 +0200

    opstart som selvstændig landmand

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
-(function(){
-    init();
+(function () {
+  init();
 
-    var g_containerInViewport;
-    function init(){
-        setStickyContainersSize();
-        bindEvents();
+  var g_containerInViewport;
+  function init() {
+    setStickyContainersSize();
+    bindEvents();
+  }
+
+  function bindEvents() {
+    window.addEventListener("wheel", wheelHandler);
+  }
+
+  function setStickyContainersSize() {
+    document.querySelectorAll(".horisontal").forEach(function (container) {
+      const stikyContainerHeight =
+        container.querySelector(".sektion").scrollWidth;
+      container.setAttribute("style", "height: " + stikyContainerHeight + "px");
+    });
+  }
+
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
+  }
+
+  function wheelHandler(evt) {
+    const containerInViewPort = Array.from(
+      document.querySelectorAll(".horisontal")
+    ).filter(function (container) {
+      return isElementInViewport(container);
+    })[0];
+
+    if (!containerInViewPort) {
+      return;
     }
 
-    function bindEvents(){
-        window.addEventListener("wheel", wheelHandler);        
+    var isPlaceHolderBelowTop =
+      containerInViewPort.offsetTop < document.documentElement.scrollTop;
+    var isPlaceHolderBelowBottom =
+      containerInViewPort.offsetTop + containerInViewPort.offsetHeight >
+      document.documentElement.scrollTop;
+    let g_canScrollHorizontally =
+      isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
+
+    if (g_canScrollHorizontally) {
+      containerInViewPort.querySelector(".sektion").scrollLeft += evt.deltaY;
     }
-
-    function setStickyContainersSize(){
-        document.querySelectorAll('.horisontal').forEach(function(container){
-            const stikyContainerHeight = container.querySelector('.sektion').scrollWidth;
-            container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
-        });
-    }
-
-    function isElementInViewport (el) {
-        const rect = el.getBoundingClientRect();
-        return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
-    }
-
-    function wheelHandler(evt){
-        
-        const containerInViewPort = Array.from(document.querySelectorAll('.horisontal')).filter(function(container){
-            return isElementInViewport(container);
-        })[0];
-
-        if(!containerInViewPort){
-            return;
-        }
-
-        var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
-        var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
-        let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
-
-        if(g_canScrollHorizontally){
-            containerInViewPort.querySelector('.sektion').scrollLeft += evt.deltaY;
-        }
-    }
+  }
 })();
